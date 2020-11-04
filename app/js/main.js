7eb882c2001c9b3104c3f4c68dda4994348f9b07
Сергей Шмигельский <siro181a@gmail.com>
2020-11-04 17:18:02 +0200

    started adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(function () {
 
+    // $('.menu__btn').on('click', function () {
+    //     $('.menu__list').slideToggle();
+    // });
 
 
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
 
 })
